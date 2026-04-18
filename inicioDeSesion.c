96753d55996e72b4fa0861ed80f2a63ee6b6e970
c-daniel02 <c.dayala@outlook.com>
2026-04-18 09:03:36 -0600

    Se ha creado el menú de opciones

--- inicioDeSesion.c
+++ inicioDeSesion.c
@@ -3,13 +3,17 @@
 
 int main(){
     char usuarioPredeterminado[15] = "daniel";
-    char contraseñaPredeterminada[15] = "Computadora";
+    char contraseñaPredeterminada[15] = "PAN";
     char usuarioIngresado[20];
     char contraseñaIngresada[20];
 
+    
     int intentos = 0, intentosRestantes = 0;
 
     int verificacionCorrecta = 0;
+    int opcionMenu = 0;
+
+    float saldoUsuario = 500;
 ////////////////////////////////////////////////////////////////////////////////////    
     printf("\n----------Inicio de sesión del banco---------\n");
 
@@ -36,6 +40,46 @@ int main(){
         }
     }
 /////////////////////////////////////////////////////////////////////////////////////// 
+    if(verificacionCorrecta == 1){
+        printf("Selecciona una opción:\n");
+        printf("1. Consultar saldo\n");
+        printf("2. Retirar\n");
+        printf("3. Depositar\n");
+        printf("4. Salir\n");
 
+    while (opcionMenu != 4){
+        printf("\n-----  Menu Operaciones  -----\n");
+        printf("Selecciona una operación para realizar:\n");
+        printf("Selecciona una opción:\n");
+        printf("1. Consultar saldo\n");
+        printf("2. Retirar\n");
+        printf("3. Depositar\n");
+        printf("4. Salir\n");
+        scanf(" %d", &opcionMenu);
+
+        switch (opcionMenu){
+        case 1: //Consulta
+            printf("--  Seleccionaste consultar  --\n");
+            printf("El saldo de tu cuenta es: %.2f", saldoUsuario);
+            break;
+        case 2: //Retirar
+            printf("--  Seleccionaste retirar  --\n");
+            
+            break;
+        case 3: //Depositar
+            printf("--  Seleccionaste depositar  --\n");
+            
+            break;
+        case 4: //Salir
+            printf("--  Seleccionaste salir  --\n");
+            break;
+        default:
+            printf("Opción no válida.\n");
+        }
+    }
+
+
+
+    }
     return 0;
 }
